e5f7d3a38df8b2ad0df065774b6e3bb947433d6e
rikupin1105 <41769991+rikupin1105@users.noreply.github.com>
2020-05-04 01:13:51 +0900

    Update Program.cs
    Webカメラから取得した画像でQRコードを読み取るようにした

--- Program.cs
+++ Program.cs
@@ -7,12 +7,33 @@ namespace QRCode
     {
         static void Main(string[] args)
         {
-            var img = Cv2.ImRead(@"画像のパス");
             using var straightQrCode = new Mat();
             var qr = new QRCodeDetector();
 
-            var QRCodeContent = qr.DetectAndDecode(img, out var point);
-            Console.WriteLine(QRCodeContent);
+
+            var capture = new VideoCapture();
+            capture.Open(0);
+
+            using (var normalWindow = new Window("QRCode"))
+            {
+                var normalFrame = new Mat();
+                var srFrame = new Mat();
+                while (true)
+                {
+                    capture.Read(normalFrame);
+                    normalWindow.ShowImage(normalFrame);
+
+
+                    var QRCodeContent = qr.DetectAndDecode(normalFrame, out var point);
+                    if (QRCodeContent != "")
+                    {
+                        Console.WriteLine(QRCodeContent);
+                    }
+
+
+                    Cv2.WaitKey(500);
+                }
+            }
         }
     }
 }
