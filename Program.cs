d65a60b7cdc34c28b3ab33bdd1c04d707a59af9d
rikupin1105 <41769991+rikupin1105@users.noreply.github.com>
2020-05-04 00:57:27 +0900

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using OpenCvSharp;
+using System;
+
+namespace QRCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var img = Cv2.ImRead(@"画像のパス");
+            using var straightQrCode = new Mat();
+            var qr = new QRCodeDetector();
+
+            var QRCodeContent = qr.DetectAndDecode(img, out var point);
+            Console.WriteLine(QRCodeContent);
+        }
+    }
+}
